88c339198301257537132dde8386bb1586fef1ff
nouf-alameri <52739110+nouf-alameri@users.noreply.github.com>
2023-03-23 11:42:37 +0400

    fv

--- src/card.js
+++ src/card.js
@@ -2,14 +2,12 @@ import React, {useState} from "react";
 
 function Card (props){
     const [num, setNum] = useState(0); 
-    const lik = (q) => {
-       setNum(num+1); 
-    }
+
     return(
     <div>
         <img src={props.img}  style={{width : "300 px", height:"300px"}}/>
         <p>{props.t}</p>
-        <button onClick={()=>{props.likes();lik()}}>Like</button>  <p>{num}</p>
+        <button onClick={()=>props.likes(props.t)}>Like</button>  <p>{props.res}</p>
     </div>
 )
 
